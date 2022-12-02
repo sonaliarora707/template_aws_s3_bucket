caa99c14c23acde505a7627497e6012336a2cd84
Sonali Arora <sonali.arora03@nmims.edu.in>
2022-12-02 20:17:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,20 @@
 provider "aws" {
   region = "eu-central-1"
 }
+
 resource "aws_s3_bucket" "b" {
-  bucket = var.bucket_name
+  bucket = "sg-snail-123"
   tags = {
-    Name        = "My bucket"
+    Name        = "My-bucket-123"
     Environment = "Dev"
+    drift_example = "v1"
   }
 }
 resource "aws_s3_bucket_acl" "example" {
   bucket = aws_s3_bucket.b.id
   acl    = "private"
 }
-variable "bucket_name" {
+
+output "bucket_name" {
+  value = aws_s3_bucket.b
 }
