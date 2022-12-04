3a17cf6df25fcec220618e74d2d078d80c4fdc15
Sonali Arora <sonaliarora2012@icloud.com>
2022-12-04 10:52:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,41 @@
+# provider "aws" {
+#   region = "eu-central-1"
+# }
+
+# resource "aws_s3_bucket" "b" {
+#   bucket = var.bucket_name
+#   tags = {
+#     Name        = "My-bucket-123"
+#     Environment = "Dev"
+#     drift_example = "v1"
+#   }
+# }
+# resource "aws_s3_bucket_acl" "example" {
+#   bucket = aws_s3_bucket.b.id
+#   acl    = "private"
+# }
+
+# variable "bucket_name" {
+  
+# }
+
+# output "bucket_drift" {
+#   value = aws_s3_bucket.b
+# }
+
 provider "aws" {
   region = "eu-central-1"
 }
-
 resource "aws_s3_bucket" "b" {
   bucket = var.bucket_name
   tags = {
-    Name        = "My-bucket-123"
+    Name        = "My bucket"
     Environment = "Dev"
-    drift_example = "v1"
   }
 }
 resource "aws_s3_bucket_acl" "example" {
   bucket = aws_s3_bucket.b.id
   acl    = "private"
 }
-
 variable "bucket_name" {
-  
-}
-
-output "bucket_drift" {
-  value = aws_s3_bucket.b
 }
