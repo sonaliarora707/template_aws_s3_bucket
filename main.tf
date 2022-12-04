9a94894bb8a6612bb7543f8f0f5416072e7566cf
Sonali Arora <sonali.arora03@nmims.edu.in>
2022-12-04 10:56:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,28 +1,3 @@
-# provider "aws" {
-#   region = "eu-central-1"
-# }
-
-# resource "aws_s3_bucket" "b" {
-#   bucket = var.bucket_name
-#   tags = {
-#     Name        = "My-bucket-123"
-#     Environment = "Dev"
-#     drift_example = "v1"
-#   }
-# }
-# resource "aws_s3_bucket_acl" "example" {
-#   bucket = aws_s3_bucket.b.id
-#   acl    = "private"
-# }
-
-# variable "bucket_name" {
-  
-# }
-
-# output "bucket_drift" {
-#   value = aws_s3_bucket.b
-# }
-
 provider "aws" {
   region = "eu-central-1"
 }
