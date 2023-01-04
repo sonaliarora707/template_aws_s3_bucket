1e9240e54068174b43d812b40ca2d7c1dde62250
Sonali Arora <sonali.arora03@nmims.edu.in>
2023-01-04 22:04:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,57 +14,48 @@
 # }
 # variable "bucket_name" {
 # }
-resource "aws_lambda_function" "example" {
-  # ... other configuration ...
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
 
-  file_system_config {
-    # EFS file system access point ARN
-    arn = aws_efs_access_point.access_point_for_lambda.arn
-
-    # Local mount path inside the lambda function. Must start with '/mnt/'.
-    local_mount_path = "/mnt/efs"
-  }
-
-  vpc_config {
-    # Every subnet should be able to reach an EFS mount target in the same Availability Zone. Cross-AZ mounts are not permitted.
-    subnet_ids         = [aws_subnet.subnet_for_lambda.id]
-    security_group_ids = [aws_security_group.sg_for_lambda.id]
-  }
-
-  # Explicitly declare dependency on EFS mount target.
-  # When creating or updating Lambda functions, mount target must be in 'available' lifecycle state.
-  depends_on = [aws_efs_mount_target.alpha]
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
 }
 
-# EFS file system
-resource "aws_efs_file_system" "efs_for_lambda" {
+resource "aws_lambda_function" "test_lambda" {
+  # If the file is not in the current working directory you will need to include a
+  # path.module in the filename.
+  filename      = "lambda_function_payload.zip"
+  function_name = "lambda_function_name"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "index.test"
+
+  # The filebase64sha256() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
+  # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
+  source_code_hash = filebase64sha256("lambda_function_payload.zip")
+
+  runtime = "nodejs16.x"
+  
   tags = {
     Owner = "stackguardian"
   }
-}
 
-# Mount target connects the file system to the subnet
-resource "aws_efs_mount_target" "alpha" {
-  file_system_id  = aws_efs_file_system.efs_for_lambda.id
-  subnet_id       = aws_subnet.subnet_for_lambda.id
-  security_groups = [aws_security_group.sg_for_lambda.id]
-}
-
-# EFS access point used by lambda file system
-resource "aws_efs_access_point" "access_point_for_lambda" {
-  file_system_id = aws_efs_file_system.efs_for_lambda.id
-
-  root_directory {
-    path = "/lambda"
-    creation_info {
-      owner_gid   = 1000
-      owner_uid   = 1000
-      permissions = "777"
+  environment {
+    variables = {
+      foo = "bar"
     }
-  }
-
-  posix_user {
-    gid = 1000
-    uid = 1000
   }
 }
